a335a9152663ef69a711d104015e6ba909d51a47
Laura <133364744+laurajhagsuUGAVGHA@users.noreply.github.com>
2024-06-13 08:34:24 -0300

    Initial commit

---
+++ lista6/lista6.cpp
@@ -0,0 +1,95 @@
+//QUINTA LISTAA
+
+#include <iostream>
+
+using namespace std;
+
+//// EXERCICIO 1 
+//// int main() {
+//	for (int i = 1; i <= 10; i++) {
+//		cout << i << "\n";
+//	}
+//	return 0;
+//}
+
+//EXERCICIO 2
+//int main() {
+//    int numero;
+//
+//    
+//    std::cout << "Informe um número: ";
+//    std::cin >> numero;
+//
+//    std::cout << "Tabuada de " << numero << ":\n";
+//    for (int i = 1; i <= 10; i++) {
+//        std::cout << numero << " x " << i << " = " << numero * i << std::endl;
+//    }
+//
+//    return 0; 
+//}
+
+////EXERCICIO 3
+//int main() {
+//   
+//    for (int i = 0; i <= 50; i += 5) {
+//        std::cout << i << std::endl; 
+//    }
+//    return 0; 
+//}
+
+////EXERCICIO 4
+//int main() {
+//    int numero; 
+//
+//    for (int i = 1; i <= 5; i++) {
+//        std::cout << "Digite o número " << i << ": ";
+//        std::cin >> numero;
+//        std::cout << "Voce digitou: " << numero << std::endl;
+//    }
+//
+//    return 0; 
+//}
+
+////EXERCICIO 5
+//int main() {
+//    int numero; 
+//
+//   
+//    for (int i = 1; i <= 5; i++) {
+//        cout << "Digite o número " << i << ": ";
+//        cin >> numero;
+//
+//       
+//        if (numero > 100) {
+//            cout << "NÚMERO ESPECIAL" << endl;
+//        }
+//        else {
+//            cout << "NÚMERO COMUM" << endl;
+//        }
+//    }
+//
+//    return 0; 
+//}
+
+//EXERCICIO 6
+//int main() {
+//    int numero;
+//
+//    for (int i = 1; i <= 5; i++) {
+//        cout << "Digite o número " << i << ": ";
+//        cin >> numero;
+//
+//        if (numero > 0) {
+//            cout << "NÚMERO POSITIVO" << endl;
+//        }
+//        else if (numero < 0) {
+//            cout << "NÚMERO NEGATIVO" << endl;
+//        }
+//        else {
+//            cout << "NÚMERO É ZERO" << endl;
+//        }
+//    }
+//
+//    return 0; 
+//}
+
